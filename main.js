5a42a7a2fa0cf8594b071f8d96bbcf00eb85e557
monolith-kh <kh.cho@monolith.co.kr>
2022-02-25 08:25:41 +0000

    show gif about display

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    fullscreen: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
