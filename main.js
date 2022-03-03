a108510d0047750f863c3d61e06804e8a303e226
monolith-kh <kh.cho@monolith.co.kr>
2022-03-03 15:16:16 +0900

    disable gpu process

--- main.js
+++ main.js
@@ -44,3 +44,5 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+app.disableHardwareAcceleration();
+app.commandLine.appendSwitch('disable-software-rasterizer');
